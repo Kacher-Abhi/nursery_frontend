97899eb928527d865ffa06c87c1c8fe64a0c5c2b
Kacher-Abhi <abhishekbkacher@gmail.com>
2024-02-16 21:49:21 +0530

    added email routes

--- app/api/email/send/route.js
+++ app/api/email/send/route.js
@@ -1,3 +1,22 @@
-export async function POST(request){
-    console.log(await request.json())
+// import { EmailTemplate } from '../../../components/EmailTemplate';
+import { Resend } from "resend";
+import { NextResponse } from "next/server";
+import YelpRecentLoginEmail from "@/components/Email/email-template";
+
+export async function POST(request) {
+    const res = await request.json();
+  const resend = new Resend(process.env.RESEND_API_KEY);
+  try {
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: res.email,
+      subject: "Hello world",
+      react: YelpRecentLoginEmail({ firstName: 'John' }),
+    });
+
+    return NextResponse.json({ data });
+    // return Response.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 }
--- components/MultiStepForm/StepForms/FormConfirmation.jsx
+++ components/MultiStepForm/StepForms/FormConfirmation.jsx
@@ -7,7 +7,6 @@ import { updateFormData } from "@/redux/slices/onboardingStudentsSlice";
 import { updatePatientFormData } from "@/redux/slices/onboardingPatientSlice";
 import { updateCaretakerFormData } from "@/redux/slices/onboardingCaretakerSlice";
 
-
 export default function FormConfirmation({ emailSend }) {
   const formData = useSelector((store) => store.onboarding.formData);
 
@@ -17,65 +16,62 @@ export default function FormConfirmation({ emailSend }) {
     dispatch(updateFormData({}));
     dispatch(updatePatientFormData({}));
     dispatch(updateCaretakerFormData({}));
+    await fetch('/api/email/send', { method: "POST", body: JSON.stringify({email : "abhishekbkacher@gmail.com"}) });
 
     console.log(formData);
   }
+
   return (
     <div>
-      {emailSend != null ? <>
-        <form className="px-12 py-4" onSubmit={processData}>
-          <div className="mb-8">
-            <h5 className="text-xl md:text-3xl font-bold text-gray-900 dark:text-white">
-              Submited Data
-            </h5>
-            <p>Your form has been successfully submitted</p>
-          </div>
-          <div className="grid gap-2 sm:grid-cols-2">
-            <div>
-              Email will be sent to {emailSend}
+      {emailSend != null ? (
+        <>
+          <form className="px-12 py-4" onSubmit={processData}>
+            <div className="mb-8">
+              <h5 className="text-xl md:text-3xl font-bold text-gray-900 dark:text-white">
+                Submited Data
+              </h5>
+              <p>Your form has been successfully submitted</p>
             </div>
-          </div>
+            <div className="grid gap-2 sm:grid-cols-2">
+              <div>Email will be sent to {emailSend}</div>
+            </div>
 
-          <button
-            type="submit"
-            className="inline-flex items-center px-5 py-2 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-900 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-slate-800 dark:bg-blue-600 dark:hover:bg-blue-700"
-          >
-            <span>
-              Confirm and Submit
-            </span>
-            <ChevronRight className="w-5 h-5 ml-2" />
-          </button>
-        </form>
-
-      </> : <>
-
-        <form className="px-12 py-4" onSubmit={processData}>
-          <div className="mb-8">
-            <h5 className="text-xl md:text-3xl font-bold text-gray-900 dark:text-white">
-              Submited Data
-            </h5>
-            <p>Your form has been successfully submitted</p>
-          </div>
-          <div className="grid gap-2 sm:grid-cols-2">
-            <code>
-              <pre>{JSON.stringify(formData, null, 2)}</pre>
-            </code>
-          </div>
-
-          <button
-            type="submit"
-            className="inline-flex items-center px-5 py-2 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-900 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-slate-800 dark:bg-blue-600 dark:hover:bg-blue-700"
-          >
-            <a href="/">
-              <span>
-                Confirm and Submit
-              </span>
+            <button
+              type="submit"
+              className="inline-flex items-center px-5 py-2 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-900 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-slate-800 dark:bg-blue-600 dark:hover:bg-blue-700"
+            >
+              <span>Confirm and Submit</span>
               <ChevronRight className="w-5 h-5 ml-2" />
-            </a>
-          </button>
-        </form>
-      </>}
+            </button>
+          </form>
+        </>
+      ) : (
+        <>
+          <form className="px-12 py-4" onSubmit={processData}>
+            <div className="mb-8">
+              <h5 className="text-xl md:text-3xl font-bold text-gray-900 dark:text-white">
+                Submited Data
+              </h5>
+              <p>Your form has been successfully submitted</p>
+            </div>
+            <div className="grid gap-2 sm:grid-cols-2">
+              <code>
+                <pre>{JSON.stringify(formData, null, 2)}</pre>
+              </code>
+            </div>
 
+            <button
+              type="submit"
+              className="inline-flex items-center px-5 py-2 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-900 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-slate-800 dark:bg-blue-600 dark:hover:bg-blue-700"
+            >
+              <a href="/">
+                <span>Confirm and Submit</span>
+                <ChevronRight className="w-5 h-5 ml-2" />
+              </a>
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
